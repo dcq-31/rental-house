1078be9d8537842f8c4cb0bc818b23a56b05ef9d
dcq <dariancapoteq@gmail.com>
2023-02-13 22:17:53 -0500

    Create home view with hero and features sections

---
+++ src/components/FeatureWidget.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+import type { Component } from "vue";
+interface IFeatureWidgetProps {
+  title: string;
+  description: string;
+  icon: Component;
+}
+const props = defineProps<IFeatureWidgetProps>();
+</script>
+<template>
+  <div class="group flex items-start space-x-4 p-2">
+    <div
+      class="grow rounded-lg bg-slate-100 p-5 ring-2 ring-transparent group-hover:ring group-hover:ring-primary group-hover:ring-opacity-60"
+    >
+      <Component :is="icon" class="h-10 w-10 text-primary" />
+    </div>
+
+    <div>
+      <div class="text-xl text-slate-700">{{ title }}</div>
+      <div class="text-base text-slate-500">{{ description }}</div>
+    </div>
+  </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,85 @@
-<script setup lang="ts">
-import TheWelcome from "../components/TheWelcome.vue";
+<script lang="ts" setup>
+import type { Component } from "vue";
+import {
+  PencilSquareIcon,
+  RocketLaunchIcon,
+  MoonIcon,
+  StarIcon,
+  ShoppingCartIcon,
+} from "@heroicons/vue/24/solid";
+import FeatureWidget from "../components/FeatureWidget.vue";
+interface IFeature {
+  title: string;
+  description: string;
+  icon: Component;
+}
+
+const FEATURES: IFeature[] = [
+  {
+    title: "Piscina",
+    description:
+      "otam nulla. Cumque aspernatur inventore ipsa quae fugit nobis dicta qui!.Cumque aspernatur inventore",
+    icon: RocketLaunchIcon,
+  },
+  {
+    title: "Garaje",
+    description:
+      "Cumque aspernatur inventore.otam nulla. Cumque aspernatur fugit nobis dicta qui!",
+    icon: StarIcon,
+  },
+  {
+    title: "Aire Frío",
+    description:
+      "otam nulla. Cumque aspernatur saepe maiores, mque aspernatur inventore ipsa quae fugit nobis dicta qui!",
+    icon: MoonIcon,
+  },
+  {
+    title: "Desayuno",
+    description:
+      "otam nulla. Ventore, aspernatur inventore ipsa quae fugit nobis dicta qui!s",
+    icon: ShoppingCartIcon,
+  },
+];
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <section
+      class="hero min-h-screen"
+      style="background-image: url(images/hero.jpg)"
+    >
+      <div class="hero-overlay bg-black bg-opacity-50"></div>
+      <div class="hero-content text-center text-white">
+        <div class="max-w-md">
+          <h1 class="mb-5 text-5xl font-bold leading-tight">
+            Bienvenido al Hostal Alita
+          </h1>
+          <p class="mb-5 text-xl">
+            Visita nuestra hermosa casa y disfruta de unas vacaciones en
+            familia.
+          </p>
+        </div>
+      </div>
+    </section>
+
+    <section class="py-16 px-2">
+      <h3 class="mb-5 text-center text-4xl font-bold text-slate-800">
+        Disfruta de:
+      </h3>
+
+      <div class="space-y-4">
+        <FeatureWidget
+          v-for="(feature, index) in FEATURES"
+          :key="`home-view-feature-${index}`"
+          v-bind="feature"
+        />
+      </div>
+    </section>
+
+    <div
+      class="btn-primary btn-circle btn-lg btn fixed bottom-6 right-6 animate-bounce text-white"
+    >
+      <PencilSquareIcon class="h-8 w-8" />
+    </div>
   </main>
 </template>
