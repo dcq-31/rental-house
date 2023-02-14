cd3ea4a7652d81513099190ab41bd67fe70597cb
dcq <dariancapoteq@gmail.com>
2023-02-13 22:16:56 -0500

    Config daisy ui theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,4 +5,21 @@ module.exports = {
     extend: {},
   },
   plugins: [require("@tailwindcss/typography"), require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#26a4b4",
+          secondary: "#e2c836",
+          accent: "#a5f3fc",
+          neutral: "#4b5563",
+          "base-100": "#ffffff",
+          info: "#3abff8",
+          success: "#36d399",
+          warning: "#fbbd23",
+          error: "#f87272",
+        },
+      },
+    ],
+  },
 };
